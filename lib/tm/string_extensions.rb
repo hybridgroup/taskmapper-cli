89d3d54b6dc41a0a9c660d5c86d1116b4f5ed0bb
Rafael George <george.rafael@gmail.com>
2013-04-02 15:13:14 +0000

    Fix passing nil to TM::StringExtensions#to_hash

--- lib/tm/string_extensions.rb
+++ lib/tm/string_extensions.rb
@@ -1,6 +1,7 @@
 module TM
   module StringExtensions
     def to_hash
+      return '' if self.nil?
       self.split(/,/).inject({}) do |hash, kv|
         arg, val = kv.split(/:/)
         hash[arg] = val
--- spec/unit/extensions/string_extensions_spec.rb
+++ spec/unit/extensions/string_extensions_spec.rb
@@ -1,10 +1,19 @@
 require_relative '../../spec_helper'
 
 describe TM::StringExtensions do 
-  let(:values) { "username:username,password:password" }
 
-  context :to_hash do 
-    subject { values.extend(TM::StringExtensions).to_hash }
-    it { should eql({"username" => "username", "password" => "password"}) }
+  describe 'when an string is send' do 
+    let(:values) { "username:username,password:password" }
+    context :to_hash do 
+      subject { values.extend(TM::StringExtensions).to_hash }
+      it { should eql({"username" => "username", "password" => "password"}) }
+    end
+  end
+
+  describe 'when nil is send' do 
+    context :to_hash do 
+      subject { nil.extend(TM::StringExtensions).to_hash }
+      it { should eql '' }
+    end
   end
 end
