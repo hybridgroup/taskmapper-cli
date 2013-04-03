2219a463a4b77910db60c020b75ef54a6b969165
Rafael George <george.rafael@gmail.com>
2013-04-03 18:14:30 +0000

    Add proper display fields for comments

--- lib/tm/format/format.rb
+++ lib/tm/format/format.rb
@@ -1,14 +1,15 @@
 module TM
   module Format
     PROJECT_KEYS = %w[id name description created_at updated_at]
-    TICKET_KEYS = %w[id status priority title resolution created_at updated_at description assignee requestor project_id]
+    TICKET_KEYS = %w[status priority title resolution assignee requestor project_id]
+    COMMENT_KEYS = %w[author body ticket_id]
 
     def before(entity); end
     def after; end
 
     def normalize_entity(entity) 
       entity.keys.inject({}) do |hash, key|
-        if PROJECT_KEYS.include?(key) || TICKET_KEYS.include?(key)
+        if PROJECT_KEYS.include?(key) || TICKET_KEYS.include?(key) || COMMENT_KEYS.include?(key)
           hash[key] = entity[key] 
         end
         hash
